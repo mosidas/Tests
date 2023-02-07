c851a6fd2a41fe57e2418a00de73b9e88f657fbe
mosida <ayshot0918@gmail.com>
2023-02-07 21:19:30 +0900

    add

---
+++ JsonTest/JsonTest/Parser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Newtonsoft.Json.Linq;
+
+namespace JsonTest
+{
+    internal class Parser
+    {
+        public static JObject Parse(string str)
+        {
+            
+            return JObject.Parse(str);
+        }
+    }
+}
---
+++ JsonTest/JsonTest/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using JsonTest;
+var input = 
+ @"
+{
+'name': [
+    'aaa',
+    'bbb',
+    'ccc'
+        ],
+'setA': {
+    'subA-1': 'valA1',
+    'subA-2': 'valA2'
+    },
+'setB': {
+    'subB-1': 'valB1',
+    'subB-2': 'valB2'
+    }
+}
+";
+
+var output = Parser.Parse(input);
+
+Console.WriteLine(output["name"]);
+Console.WriteLine(output["setA"]);
+
+Console.ReadLine();
